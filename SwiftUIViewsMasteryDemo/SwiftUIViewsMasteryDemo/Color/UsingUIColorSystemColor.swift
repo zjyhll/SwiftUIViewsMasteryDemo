443bbf869601b4680daaa445de410d756203335e
RecherJ <ygliusz@foxmail.com>
2021-07-15 18:49:59 +0800

    color using UIColor system color in dark mode

--- SwiftUIViewsMasteryDemo/SwiftUIViewsMasteryDemo/Color/UsingUIColorSystemColor.swift
+++ SwiftUIViewsMasteryDemo/SwiftUIViewsMasteryDemo/Color/UsingUIColorSystemColor.swift
@@ -17,72 +17,89 @@ struct UsingUIColorSystemColor: View {
                 .foregroundColor(.gray)
             Text("Using UIColor.system will give you an even wider range of colors that will change for light and dark modes.")
                 .frame(maxWidth: .infinity)
+                .font(.title)
                 .padding()
                 .background(Color.orange)
                 .layoutPriority(1)
 
-            HStack {
-                RoundedRectangle(cornerRadius: 10)
-                    .fill(Color(UIColor.systemGray))
-                    .overlay(Text("systemGray"))
-                RoundedRectangle(cornerRadius: 10)
-                    .fill(Color(UIColor.systemGray2))
-                    .overlay(Text("systemGray2"))
-                RoundedRectangle(cornerRadius: 10)
-                    .fill(Color(UIColor.systemGray3))
-                    .overlay(Text("systemGray3"))
+            VStack {
+                HStack {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color(UIColor.systemGray))
+                        .overlay(Text("systemGray"))
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color(UIColor.systemGray2))
+                        .overlay(Text("systemGray2"))
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color(UIColor.systemGray3))
+                        .overlay(Text("systemGray3"))
+                }
+                HStack {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color(UIColor.systemGray4))
+                        .overlay(Text("systemGray4"))
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color(UIColor.systemGray5))
+                        .overlay(Text("systemGray5"))
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color(UIColor.systemGray6))
+                        .overlay(Text("systemGray6"))
+                }
+                HStack {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color(UIColor.systemTeal))
+                        .overlay(Text("systemTeal"))
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color(UIColor.systemIndigo))
+                        .overlay(Text("systemIndigo"))
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color(UIColor.systemBlue))
+                        .overlay(Text("systemBlue"))
+                }
+                HStack {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color(UIColor.systemRed))
+                        .overlay(Text("systemRed"))
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color(UIColor.systemPurple))
+                        .overlay(Text("systemPurple"))
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color(UIColor.systemPink))
+                        .overlay(Text("systemPink"))
+                }
+                HStack {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color(UIColor.systemYellow))
+                        .overlay(Text("systemYellow"))
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color(UIColor.systemOrange))
+                        .overlay(Text("systemOrange"))
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color(UIColor.systemGreen))
+                        .overlay(Text("systemGreen"))
+                }
             }
-            HStack {
-                RoundedRectangle(cornerRadius: 10)
-                    .fill(Color(UIColor.systemGray4))
-                    .overlay(Text("systemGray4"))
-                RoundedRectangle(cornerRadius: 10)
-                    .fill(Color(UIColor.systemGray5))
-                    .overlay(Text("systemGray5"))
-                RoundedRectangle(cornerRadius: 10)
-                    .fill(Color(UIColor.systemGray6))
-                    .overlay(Text("systemGray6"))
-            }
-            HStack {
-                RoundedRectangle(cornerRadius: 10)
-                    .fill(Color(UIColor.systemTeal))
-                    .overlay(Text("systemTeal"))
-                RoundedRectangle(cornerRadius: 10)
-                    .fill(Color(UIColor.systemIndigo))
-                    .overlay(Text("systemIndigo"))
-                RoundedRectangle(cornerRadius: 10)
-                    .fill(Color(UIColor.systemBlue))
-                    .overlay(Text("systemBlue"))
-            }
-            HStack {
-                RoundedRectangle(cornerRadius: 10)
-                    .fill(Color(UIColor.systemRed))
-                    .overlay(Text("systemRed"))
-                RoundedRectangle(cornerRadius: 10)
-                    .fill(Color(UIColor.systemPurple))
-                    .overlay(Text("systemPurple"))
-                RoundedRectangle(cornerRadius: 10)
-                    .fill(Color(UIColor.systemPink))
-                    .overlay(Text("systemPink"))
-            }
-            HStack {
-                RoundedRectangle(cornerRadius: 10)
-                    .fill(Color(UIColor.systemYellow))
-                    .overlay(Text("systemYellow"))
-                RoundedRectangle(cornerRadius: 10)
-                    .fill(Color(UIColor.systemOrange))
-                    .overlay(Text("systemOrange"))
-                RoundedRectangle(cornerRadius: 10)
-                    .fill(Color(UIColor.systemGreen))
-                    .overlay(Text("systemGreen"))
-            }
+
             Text("Many colors here are the same as SwiftUI colors. But the system colors give you multiple grays, Teal and Indigo.")
+                .frame(maxWidth: .infinity)
+                .font(.title)
+                .padding()
+                .background(Color.orange)
+                .layoutPriority(1)
         }
     }
 }
 
 struct UsingUIColorSystemColor_Previews: PreviewProvider {
     static var previews: some View {
-        UsingUIColorSystemColor()
+        Group {
+            UsingUIColorSystemColor()
+                .preferredColorScheme(.dark)
+            UsingUIColorSystemColor()
+        }
     }
 }
+
+// Notice how the system gray colors change from light and dark mode.
+// Keep this in mind if you are using them in your design to make sure
+// your UI looks good in both dark and light modes.
