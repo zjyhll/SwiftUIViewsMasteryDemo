af7495b05361e7031ad1089116392d12d70c29e9
RecherJ <ygliusz@foxmail.com>
2021-07-15 17:23:44 +0800

    add project (move exist project from kavsoft.dev-samples repository)

---
+++ SwiftUIViewsMasteryDemo/SwiftUIViewsMasteryDemo/SwiftUIViewsMasteryDemoApp.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIViewsMasteryDemoApp.swift
+//  TestPadding
+//
+//  Created by RecherJ on 2021/7/4.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIViewsMasteryDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+//            ContentView()
+            TrimSolidShape()
+        }
+    }
+}
