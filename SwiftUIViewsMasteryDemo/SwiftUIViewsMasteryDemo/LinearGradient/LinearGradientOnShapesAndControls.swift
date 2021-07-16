4f9bb18f15f966822d335c299b61591c41763593
RecherJ <ygliusz@foxmail.com>
2021-07-16 21:44:50 +0800

    linear gradient on shapes and controls

---
+++ SwiftUIViewsMasteryDemo/SwiftUIViewsMasteryDemo/LinearGradient/LinearGradientOnShapesAndControls.swift
@@ -0,0 +1,54 @@
+//
+//  LinearGradientOnShapesAndControls.swift
+//  SwiftUIViewsMasteryDemo
+//
+//  Created by RecherJ on 2021/7/16.
+//
+
+import SwiftUI
+
+struct LinearGradientOnShapesAndControls: View {
+    let gradientColors = Gradient(colors: [.pink, .purple])
+
+    var body: some View {
+        let linearGradient = LinearGradient(gradient: gradientColors, startPoint: .top, endPoint: .bottom)
+        return VStack(spacing: 20) {
+            Text("LinearGradient")
+                .font(.largeTitle)
+            Text("On Shapes & Controls")
+                .foregroundColor(.gray)
+            Text("You can apply gradients to shapes and controls using the fill and background modifiers respectively.")
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(Color.red)
+                .shadow(radius: 10)
+
+            Capsule()
+                .fill(linearGradient)
+                .frame(height: 100)
+                .padding()
+
+            Button(action: {}) {
+                Text("Button")
+                    .foregroundColor(.white)
+                    .padding()
+            }
+            .background(linearGradient)
+            .cornerRadius(15)
+
+            Text("Use strokeBorder to apply a gradient to the borders of shapes.")
+                .padding(.horizontal)
+            Circle()
+                .strokeBorder(linearGradient, lineWidth: 20)
+                .frame(height: 200)
+        }
+        .font(.title)
+    }
+}
+
+struct LinearGradientOnShapesAndControls_Previews: PreviewProvider {
+    static var previews: some View {
+        LinearGradientOnShapesAndControls()
+            .preferredColorScheme(.dark)
+    }
+}
