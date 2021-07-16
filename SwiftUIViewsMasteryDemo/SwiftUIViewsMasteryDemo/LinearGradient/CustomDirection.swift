e847230af94d55f573a8dfe4281002ab944687fa
RecherJ <ygliusz@foxmail.com>
2021-07-16 22:01:26 +0800

    linear gradient custom direction

--- SwiftUIViewsMasteryDemo/SwiftUIViewsMasteryDemo/LinearGradient/CustomDirection.swift
+++ SwiftUIViewsMasteryDemo/SwiftUIViewsMasteryDemo/LinearGradient/CustomDirection.swift
@@ -1,5 +1,5 @@
 //
-//  GradientDirection.swift
+//  CustomDirection.swift
 //  SwiftUIViewsMasteryDemo
 //
 //  Created by RecherJ on 2021/7/16.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct GradientDirection: View {
+struct CustomDirection: View {
     let gradientColors = Gradient(colors: [.pink, .black])
 
     var body: some View {
@@ -17,45 +17,45 @@ struct GradientDirection: View {
             Text("Gradient Direction")
                 .font(.title)
                 .foregroundColor(.gray)
-            Text("Gradient direction is set using the startPoint and endPoint parameters. You use the UnitPoint type for the parameter values. Points include:")
+            Text("If you don't want to use the preset UnitPoints then you can create different points using an X and Y coordinate. The X and Y values will range from 0 to 1.")
                 .frame(maxWidth: .infinity)
-                .padding()
                 .font(.title)
+                .padding()
                 .background(Color.pink)
+
             HStack(spacing: 20) {
                 Rectangle()
                     .fill(LinearGradient(
                             gradient: gradientColors,
-                            startPoint: .leading,
-                            endPoint: .bottom
+                            startPoint: UnitPoint(x: 0, y: 0.25),
+                            endPoint: UnitPoint(x: 1, y: 0.75)
                         )
                     )
                     .frame(width: 100, height: 100)
                 Rectangle()
                     .fill(LinearGradient(
                             gradient: gradientColors,
-                            startPoint: .bottomLeading,
-                            endPoint: .topTrailing
+                        startPoint: UnitPoint(x: 0.65, y: 0.65),
+                        endPoint: UnitPoint(x: 0.8, y: 0.75)
                         )
                     )
                     .frame(width: 100, height: 100)
                 Rectangle()
                     .fill(LinearGradient(
                             gradient: gradientColors,
-                            startPoint: .bottomTrailing,
-                            endPoint: .leading
+                        startPoint: UnitPoint(x: 0.9, y: 0.8),
+                        endPoint: UnitPoint(x: 0.25, y: 0.15)
                         )
                     )
                     .frame(width: 100, height: 100)
             }
-
         }
     }
 }
 
-struct GradientDirection_Previews: PreviewProvider {
+struct CustomDirection_Previews: PreviewProvider {
     static var previews: some View {
-        GradientDirection()
+        CustomDirection()
             .preferredColorScheme(.dark)
     }
 }
