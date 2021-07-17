b9125ec5b114cb3465a152f421df5de38b0e7c31
RecherJ <ygliusz@foxmail.com>
2021-07-17 21:25:21 +0800

    max height and width with infinity to make push-out views

---
+++ SwiftUIViewsMasteryDemo/SwiftUIViewsMasteryDemo/Frame/MaxHeightAndWidthWithInfinityToMakePushOutViews.swift
@@ -0,0 +1,42 @@
+//
+//  MaxHeightAndWidthWithInfinityToMakePushOutViews.swift
+//  SwiftUIViewsMasteryDemo
+//
+//  Created by RecherJ on 2021/7/17.
+//
+
+import SwiftUI
+
+struct MaxHeightAndWidthWithInfinityToMakePushOutViews: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("Frame")
+                .font(.largeTitle)
+            Text("Max Sizes")
+                .foregroundColor(.gray)
+            Text("Using .infinity is a great way to turn pull-in views into push-out views.")
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(Color.pink)
+                .foregroundColor(.white)
+
+            Text("Pull-In View")
+            Image(systemName: "arrow.right.arrow.left")
+                .padding()
+                .background(Color.pink)
+            Text("Now a Push-Out View")
+            Image(systemName: "arrow.right.arrow.left")
+                .padding()
+                // Using .infinity for max width and height makes this a push-out view now
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color.pink)
+        }
+        .font(.title)
+    }
+}
+
+struct MaxHeightAndWidthWithInfinityToMakePushOutViews_Previews: PreviewProvider {
+    static var previews: some View {
+        MaxHeightAndWidthWithInfinityToMakePushOutViews()
+    }
+}
