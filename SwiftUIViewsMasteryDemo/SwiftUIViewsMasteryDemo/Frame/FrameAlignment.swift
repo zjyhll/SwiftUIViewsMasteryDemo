85e6bc2aed543defc9f971f5e9cac50683334b91
RecherJ <ygliusz@foxmail.com>
2021-07-17 18:59:03 +0800

    frame alignment

--- SwiftUIViewsMasteryDemo/SwiftUIViewsMasteryDemo/Frame/FrameAlignment.swift
+++ SwiftUIViewsMasteryDemo/SwiftUIViewsMasteryDemo/Frame/FrameAlignment.swift
@@ -13,8 +13,39 @@ struct FrameAlignment: View {
             Text("Frame")
                 .font(.largeTitle)
             Text("Alignment")
+                .font(.title)
                 .foregroundColor(.gray)
             Text("The frame modifier allows you to align the content widthin a frame (if there is avaliable space). The alignment won't work on views push out to fill all available space, like shapes.")
+                .frame(maxWidth: .infinity)
+                .padding()
+                .font(.title)
+                .background(Color.red)
+                .foregroundColor(.white)
+            HStack {
+                Button(action: {}) {
+                    Text("Button")
+                }
+                .frame(width: 150, height: 150, alignment: .top)
+                .border(Color.pink)
+
+                Button(action: {}) {
+                    Text("Button")
+                }
+                .frame(width: 150, height: 150, alignment: .leading)
+                .border(Color.pink)
+            }
+            HStack {
+                Button(action: {}) {
+                    Text("Button")
+                }
+                .frame(width: 150, height: 150, alignment: .leading)
+                .border(Color.pink)
+                Button(action: {}) {
+                    Text("Button")
+                }
+                .frame(width: 150, height: 150, alignment: .bottom)
+                .border(Color.pink)
+            }
         }
     }
 }
