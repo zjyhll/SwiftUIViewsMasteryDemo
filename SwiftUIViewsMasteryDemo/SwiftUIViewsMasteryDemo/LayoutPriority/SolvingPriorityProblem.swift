a568b2f25df44d7938260a8aa17524721aeb9d34
RecherJ <ygliusz@foxmail.com>
2021-07-18 21:05:05 +0800

    solving problem (still a problem)

--- SwiftUIViewsMasteryDemo/SwiftUIViewsMasteryDemo/LayoutPriority/SolvingPriorityProblem.swift
+++ SwiftUIViewsMasteryDemo/SwiftUIViewsMasteryDemo/LayoutPriority/SolvingPriorityProblem.swift
@@ -28,11 +28,13 @@ struct SolvingPriorityProblem: View {
                 // You want this text to expand
                 Text("Dolor amet man braid bushwick salvia, vinyl art party pickled exercitation.")
                     .foregroundColor(.yellow)
+                    // Make highest priority
+                    .layoutPriority(100)
                 Text("Laborum bitters vice, letterpress pariatur pour-over iceland.")
                     .font(.caption)
             }
             .padding()
-            Text("The text view in the middle (yellow) is getting truncated and you want to expand this. what can you do? (Notice the text views with yellow have priority.)")
+            Text("This still didn't solve the problem. Why?")
                 .frame(maxWidth: .infinity)
                 .padding()
                 .background(Color.yellow)
