0c4b31da74eaeb79a09dc53efa218a621dc1b23d
RecherJ <ygliusz@foxmail.com>
2021-07-18 21:09:36 +0800

    solving problem - solution

--- SwiftUIViewsMasteryDemo/SwiftUIViewsMasteryDemo/LayoutPriority/SolvingPriorityProblem.swift
+++ SwiftUIViewsMasteryDemo/SwiftUIViewsMasteryDemo/LayoutPriority/SolvingPriorityProblem.swift
@@ -12,34 +12,34 @@ struct SolvingPriorityProblem: View {
         VStack(spacing: 10) {
             Text("Layout Priority")
                 .font(.largeTitle)
-            Text("Solving Priorities")
+            Text("Solution")
                 .font(.title)
                 .foregroundColor(.gray)
-            Text("Take a look at this problem here:")
+            Text("3. Now, this text view has third highest priority.")
                 .frame(maxWidth: .infinity)
                 .padding()
                 .background(Color.yellow)
                 .foregroundColor(.black)
                 .layoutPriority(1) // Second Priority
-
+            Text("1. This VStack has the HIGHEST layout priority:")
             VStack(alignment: .leading) {
                 Text("Lorem Ipsum")
                     .font(.largeTitle)
                 // You want this text to expand
                 Text("Dolor amet man braid bushwick salvia, vinyl art party pickled exercitation.")
                     .foregroundColor(.yellow)
-                    // Make highest priority
-                    .layoutPriority(100)
+                    .layoutPriority(1) // Highest priority among these sibling views
                 Text("Laborum bitters vice, letterpress pariatur pour-over iceland.")
                     .font(.caption)
             }
             .padding()
+            .layoutPriority(3) // Highest priority among these sibling views
             Text("This still didn't solve the problem. Why?")
                 .frame(maxWidth: .infinity)
                 .padding()
                 .background(Color.yellow)
                 .foregroundColor(.black)
-                .layoutPriority(2) // First Priority (highest number)
+                .layoutPriority(2) // Second highest
         }
         .font(.title)
     }
